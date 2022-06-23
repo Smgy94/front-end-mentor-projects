f082d58b363b4fe0a9126231218c42c717092416
Smgy94 <shanemcgeown@gmail.com.com>
2022-06-23 22:35:26 +0100

    commit

---
+++ base-apparel-coming-soon-master/app.js
@@ -0,0 +1,29 @@
+function validation() {
+    var form = document.getElementById("form");
+    var email = document.getElementById("email").value;
+    var text = document.getElementById("text");
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (email.match(pattern)) {
+        form.classList.add("valid");
+        form.classList.remove("invalid");
+        text.innerHTML = "";
+        text.style.color = "#00ff00";
+         document.getElementById("email").style.outline = "";
+    }
+    else {
+          form.classList.remove("valid");
+           form.classList.add("invalid");
+           text.innerHTML = "Please provide a valid email";
+        text.style.color = "#f96262";
+        document.getElementById("email").style.outline = "2px solid #f96262";
+    }
+
+    if (email == "") {
+        form.classList.remove("valid");
+        form.classList.remove("invalid");
+        text.innerHTML = "";
+        text.style.color = "#00ff00";
+         document.getElementById("email").style.outline = "";
+    }
+}
